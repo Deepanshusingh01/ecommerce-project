e0558292cba856c498bf22f059820eb6315b38c5
gauravpundir15 <gauravpundir@mobikasa.com>
2023-05-01 12:06:23 +0530

    frontend modified

---
+++ frontend/src/APIs/APIs.js
@@ -0,0 +1,28 @@
+const data = {
+    products: [
+        {
+            id:1,
+            name:"Apple iPhone 14 Pro Max",
+            price: 127999,
+            imgPath: '/winter.jpg',
+            description:"This is an Apple company product. iPhone 14 pro max is fastest phone in the world"
+        },
+        {
+            id:2,
+            name:"Apple iPhone 12 Pro Max",
+            price: 54999,
+            imgPath: '/summer.jpg',
+            description:"This is an Apple company product. iPhone 12 pro max is fastest phone in the world"
+        },
+        {
+            id:3,
+            name:"Apple iPhone 13 Pro Max",
+            price: 99999,
+            imgPath: '/winter.jpg',
+            description:"This is an Apple company product. iPhone 13 pro max is fastest phone in the world"
+        }
+    ]
+};
+
+export default data;
+
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Dashboard.css';
+import APIs from '../APIs/APIs';
+// import { Context } from '../App';
+const products = APIs.products;
+console.log(products);
+
 
 function Dashboard() {
+
+    const [quantity, setQuantity] = useState(1);
+
+    function quantityIncrease() {
+        setQuantity(1);
+    }
+
+    function quantityDecrease() {
+        if (quantity > 1) {
+            setQuantity(quantity - 1);
+        }
+    }
+
+    function addToCart() {
+        setQuantity(quantity + 1);
+    }
+
     return (
         <>
-            <div className="container-xxl text-center">
-                <div className="row">
-                    <div className="custom col-3">
-                        1 of 2
-                    </div>
-                    <div className="custom col-8">
-                        2 of 2
-                    </div>
-                </div>
-            </div>
+            {
+                products.map((products, index) => {
+                    return (
+                        <div className="card">
+                            <img className="card-img-top" src={products.imgPath} alt="" />
+                            <div className="card-body">
+                                <h5 className="card-title">{products.name}</h5>
+                                <p className="card-text">{products.description}</p>
+                                <div className="quantity-div">
+                                    <input type="button" className="quantity btn-primary" value="-" onClick={quantityDecrease} />
+                                    <input type="button" className="quantity" value={0} />
+                                    <input type="button" className="quantity btn-primary" value="+" onClick={quantityIncrease} />
+                                </div>
+                                <div className='rupees'>
+                                    <h5>Price</h5><span><h5><i>&#8377;</i>{products.price}/-</h5></span>
+                                </div>
+                                <div>
+                                    <button className="add-to-cart btn btn-primary" onClick={addToCart}>Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </>
     );
 }
